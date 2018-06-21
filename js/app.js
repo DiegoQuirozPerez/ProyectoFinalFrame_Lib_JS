9aafbdf36721d6ae99036b9dccd11560a8a1ade0
Diego <dquiroz1988@gmail.com>
2018-06-20 23:48:21 -0400

    funcion llenar
    se detalla la funcion llenar

--- js/app.js
+++ js/app.js
@@ -50,3 +50,23 @@ function timer(){
 function callback(){
   $(".panel-score").animate({width:'100%'}, 4000);
 }
+
+function llenar_img(){
+  i=i+1
+  var numero = 0;
+  var imagen;
+  $(".elemento").draggable({disable: true});
+  if(i<8){
+    for (var j=1; j<8; j++){
+      if($(".col"+j).children("img:nth-child("+i")").html()==null){
+        numero = Math.foor(Math.randon() * 4) + 1;
+        imagen = "image/"+numero+".png";
+        $(".col-"+j).prepend("<img src="+imagen+"class='elemento'/>").css("justify-content","flex")
+      }
+    }
+  }
+  if(i==8){
+    clearInterval(intervalo); //desactiva la funcion llenar_img
+    eliminar=setInterval(function(){eliminarhorver()},150) //activa la funcion eliminarhorver
+  }
+}
